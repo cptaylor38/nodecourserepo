671b835e2cb99899ba16a72979fe062d0112a1b7
Corbin Taylor <cptaylor38@yahoo.com>
2021-02-23 01:03:03 -0500

    section 2-29 - added autocannon for performance testing

--- index.js
+++ index.js
@@ -1,53 +1,45 @@
 const cluster = require('cluster');
 
+//Autocannon docs https://github.com/mcollina/autocannon#install
+//To manually kill the node server
+//ps aux | grep node
+//kill -9 [second number from left for process id]
+//command to run autocannon
+//autocannon -c [number of connections] -d [the number of seconds to run autocannon] -p [number of pipelined requests to use] [url of server]
+
 //first time we execute file, is file being executed in master mode?
 if(cluster.isMaster){
     cluster.fork();
     //Cause file to be executed again, but in child mode.
-    cluster.fork();
-    cluster.fork();
-    cluster.fork();
 }
 else {
-//Child - acts like a server and does nothing else.
+    //Child - acts like a server and does nothing else.
 
-const express = require('express');
-const app = express();
+    const express = require('express');
+    const app = express();
+    const crypto = require('crypto');
+    
+    app.get('/', (req, res)=> {
+        crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', ()=> {
+            res.send('worker id: ' + cluster.worker.id);
+        })
+    })
+    app.get('/1', (req, res)=> {
+        res.send('worker id: ' + cluster.worker.id);
+    })
+    app.get('/2', (req, res)=> {
+        res.send('worker id: ' + cluster.worker.id);
+    })
+    app.get('/3', (req, res)=> {
+        res.send('worker id: ' + cluster.worker.id);
+    })
+    app.get('/4', (req, res)=> {
+        res.send('worker id: ' + cluster.worker.id);
+    })
 
-function doWork(duration){
-    //purpose of this function is to use as much cpu processing power for a set duration
-    const start = Date.now();
-    while(Date.now() - start < duration){
-        //Attempting to run the while loop as fast as possible.
-        //No other actions can be completed while this is running.
-    }
-}
-
-app.get('/', (req, res)=> {
-    doWork(5000);
-    //This gets executed during the event loop.
-    res.send('worker id: ' + cluster.worker.id);
-})
-
-app.get('/1', (req, res)=> {
-    //not working on Windows - looking for alternative.
-    res.send('worker id: ' + cluster.worker.id);
-})
-
-app.get('/2', (req, res)=> {
-    res.send('worker id: ' + cluster.worker.id);
-})
-
-app.get('/3', (req, res)=> {
-    res.send('worker id: ' + cluster.worker.id);
-})
-app.get('/4', (req, res)=> {
-    res.send('worker id: ' + cluster.worker.id);
-})
-
-//Testing to see how many I needed on windows to show different workers. Seemed to work 
-//          at * + 4 additional.
+    //Testing to see how many I needed on windows to show different workers. Seemed to work 
+    //          at * + 4 additional.
 
 
-app.listen(3000);
+    app.listen(3000);
 }
--- nodePerformanceNotes.js
+++ nodePerformanceNotes.js
@@ -26,3 +26,13 @@
 //      node internally goes back to the file.js and executes it a second time and starts up worker instance
 
 //When forking off to additional worker instances, isMaster is set to false.
+
+
+// function doWork(duration){
+//     //purpose of this function is to use as much cpu processing power for a set duration
+//     const start = Date.now();
+//     while(Date.now() - start < duration){
+//         //Attempting to run the while loop as fast as possible.
+//         //No other actions can be completed while this is running.
+//     }
+// }
